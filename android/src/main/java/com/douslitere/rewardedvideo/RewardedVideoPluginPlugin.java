b4288f7f0413a5ad77c604528c78649379ad894c
lolobot <bonkwen.anons@gmail.com>
2025-06-21 17:09:41 +0200

    ok

--- android/src/main/java/com/douslitere/rewardedvideo/RewardedVideoPluginPlugin.java
+++ android/src/main/java/com/douslitere/rewardedvideo/RewardedVideoPluginPlugin.java
@@ -1,22 +1,65 @@
 package com.douslitere.rewardedvideo;
 
+import android.app.Activity;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import com.ironsource.mediationsdk.LevelPlay;
+import com.ironsource.mediationsdk.LevelPlayInitRequest;
+import com.ironsource.mediationsdk.LevelPlayInitListener;
+import com.ironsource.mediationsdk.LevelPlayConfiguration;
+import com.ironsource.mediationsdk.LevelPlayInitError;
+import com.ironsource.mediationsdk.IronSource;
+import com.ironsource.mediationsdk.logger.IronLog;
+import com.ironsource.mediationsdk.model.Placement;
+
+import java.util.Arrays;
+
 @CapacitorPlugin(name = "RewardedVideoPlugin")
 public class RewardedVideoPluginPlugin extends Plugin {
 
-    private RewardedVideoPlugin implementation = new RewardedVideoPlugin();
+    @PluginMethod
+    public void initialize(PluginCall call) {
+        String appKey = call.getString("appKey");
+        String userId = call.getString("userId", "user123");
+
+        Activity activity = getActivity();
+
+        IronSource.setMetaData(activity, "Facebook_IS_CacheFlag", "IMAGE"); // Meta support
+        IronSource.setMetaData("is_test_suite", "enable"); // facultatif
+
+        LevelPlayInitRequest initRequest = new LevelPlayInitRequest.Builder(appKey)
+                .withLegacyAdFormats(Arrays.asList(LevelPlay.AdFormat.REWARDED))
+                .withUserId(userId)
+                .build();
+
+        LevelPlay.init(activity.getApplicationContext(), initRequest, new LevelPlayInitListener() {
+            @Override
+            public void onInitFailed(@NonNull LevelPlayInitError error) {
+                call.reject("Init failed: " + error.getMessage());
+            }
+
+            @Override
+            public void onInitSuccess(LevelPlayConfiguration configuration) {
+                call.resolve();
+            }
+        });
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    public void showRewarded(PluginCall call) {
+        if (IronSource.isRewardedVideoAvailable()) {
+            IronSource.showRewardedVideo();
+            call.resolve();
+        } else {
+            call.reject("Rewarded video not available");
+        }
     }
 }
